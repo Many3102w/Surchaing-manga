e06a890c2c77fe8464748c8618155450c3485e36
Emidev <emidevdeveloper@gmail.com>
2024-05-11 00:15:31 -0600

    Segundas correcciones

--- webcomics/urls.py
+++ webcomics/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home/", views.home),
+    path('', views.home, name="home"),
     path("manga/", views.manga_view, name="manga"),
     path("about/", views.about, name="about")
 ]
