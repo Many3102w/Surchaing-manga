5f3373801ed71394835ae08f8ea5d99b1b0084a6
Emidev <emidevdeveloper@gmail.com>
2024-05-24 18:44:21 -0600

    sintatixs corregida

--- webcomic/settings.py
+++ webcomic/settings.py
@@ -140,7 +140,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'djatgo.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
