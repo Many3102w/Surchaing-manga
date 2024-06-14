536e87a229faefb12b363d2b29b047297ef9c99d
Emidev <emidevdeveloper@gmail.com>
2024-06-13 23:30:46 -0600

    ckeditor installed

--- webcomic/settings.py
+++ webcomic/settings.py
@@ -70,12 +70,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'webcomics'
+    'webcomics',
+    'ckeditor'
 ]
 
-MORE_APPS = [
-    #Instalar aplicaciones adicionales
-]
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'full',
+        'height': 300,
+        'width': 300,
+    },
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -181,6 +187,8 @@ MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'))
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 
 if not DEBUG:
     pass
